071b7f17a64c6ab91b4593ddcf953fe988ed65a9
Vadzim Dzianisik <wadik.nhl@gmail.com>
2021-09-24 22:15:06 +0300

    add cra ts files 01

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {value01_01} from "./01/01";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let a = value01_01;
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
